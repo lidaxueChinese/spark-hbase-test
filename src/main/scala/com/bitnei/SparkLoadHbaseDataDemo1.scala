085bf5cd507fd62cdaa71735cb9ab07e33ed9c5e
lidaxue <lidaxue@bitnei.cn>
2019-05-24 13:12:21 +0800

    换用Row代替case class 存储parquet文件

--- src/main/scala/com/bitnei/SparkLoadHbaseDataDemo1.scala
+++ src/main/scala/com/bitnei/SparkLoadHbaseDataDemo1.scala
@@ -1,18 +1,19 @@
 package com.bitnei
 
-import java.util
-
 import com.bitnei.model.RealInfoModel
-import org.apache.hadoop.hbase.{Cell, CellUtil, HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.client.Scan
-import org.apache.hadoop.hbase.filter._
 import org.apache.hadoop.hbase.spark.HBaseContext
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.hadoop.hbase.client.Result
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+import scala.collection.JavaConverters._
 
 /**
+  * 通过fuzzy，构建每个小时构建数据
   * Created by lidaxue@bitnei.cn on 2019/5/9.
   */
 class SparkLoadHbaseDataDemo1(@transient private var sc:SparkContext,executeDate:String) extends Serializable{
@@ -20,37 +21,36 @@ class SparkLoadHbaseDataDemo1(@transient private var sc:SparkContext,executeDate
   val sparkSession = SparkSession.builder().enableHiveSupport().config(sc.getConf).getOrCreate()
 
 
-  def loadDataFromHbase(): Unit ={
+  def loadDataFromHbase1(): Unit ={
 
     val hbaseConf = HBaseConfiguration.create()
     hbaseConf.set("hbase.zookeeper.property.clientPort", "2181")
     hbaseConf.set("hbase.zookeeper.quorum", "cnqycspser01,cnqycspser02,cnqycspser03")
     hbaseConf.set("hbase.master", "cnqycspser01:60000")
     hbaseConf.setLong("hbase.rpc.timeout", 5 * 60 * 1000L)
-
-    val timePeriod = DateUtil.getMinAndMaxTimestamp(executeDate)
-    val startTimestamp = timePeriod._1
-    val endTimestamp = timePeriod._2
+    val schema = StructType(Array(StructField("VID",StringType,true),StructField("str7615",StringType,true)))
+    val executeDateTimestamp = DateUtil.getTimestampFromDateStr(executeDate);
     val hBaseContext = new HBaseContext(sc,hbaseConf)
     val scan = new Scan()
     scan.setCacheBlocks(false)
     scan.setCaching(100)
-    val timestampPrefix = DateUtil.getFilterDatePrefix(executeDate)
-    //fuzzy scan  HbaseRowkeyFilter.getFuzzyRowkeyFilter(timestampPrefix._1,timestampPrefix._2)
-    // regex scan getRegexRowkeyFilter()
-    scan.setFilter(HbaseRowkeyFilter.getFuzzyRowkeyFilter(timestampPrefix._1,timestampPrefix._2))
+    for(hour <- 0 until 2) {
+      val timeIndexList = DateUtil.getTimestampList(executeDateTimestamp,hour).toList.map(value => new Integer(value)).asJava
+      scan.setFilter(HbaseRowkeyFilter.getFuzzyRowkeyFilter(timeIndexList))
 
-    val scanRdd = hBaseContext.hbaseRDD(TableName.valueOf("realinfo"),scan)
-    val finalRdd = scanRdd.filter(f=>{
-       val rowKey = Bytes.toString(f._1.copyBytes())
-       getSpecialDayData(rowKey,startTimestamp,endTimestamp)
-    }).map(r=>{
-       generateRealInfoModelFromHbaseValue(r._2)
-    })
+      val scanRdd = hBaseContext.hbaseRDD(TableName.valueOf("realinfo"), scan)
+      val finalRdd = scanRdd.filter(f => {
+        val rowKey = Bytes.toString(f._1.copyBytes())
+        getSpecialDayData(rowKey,executeDateTimestamp,hour)
+      }).map(r => {
+        generateRowFromHbaseValue(r._2)
+      })
 
-    import sparkSession.implicits._
-    finalRdd.toDF().write.mode(SaveMode.Overwrite).parquet(s"/tmp/spark/vehicle/result/term_result/${executeDate}")
+      import sparkSession.implicits._
+      //finalRdd.toDF().write.mode(SaveMode.Overwrite).parquet(s"/tmp/spark/vehicle/result/term_result/${executeDate}-1/${hour}")
+      sparkSession.createDataFrame(finalRdd,schema).write.mode(SaveMode.Overwrite).parquet(s"/tmp/spark/vehicle/result/term_result/${executeDate}-1/${hour}")
 
+    }
   }
 
   def generateRealInfoModelFromHbaseValue(result:Result): RealInfoModel ={
@@ -69,12 +69,28 @@ class SparkLoadHbaseDataDemo1(@transient private var sc:SparkContext,executeDate
 
   }
 
+  def generateRowFromHbaseValue(result:Result): Row ={
+    val cfMap = result.getFamilyMap(Bytes.toBytes("cf"))
+    val VID = if(cfMap.containsKey(Bytes.toBytes("VID"))){
+      Bytes.toString(cfMap.get(Bytes.toBytes("VID")))
+    }else{
+      "-1"
+    }
+    val str7615 =  if(cfMap.containsKey(Bytes.toBytes("7615"))){
+      Bytes.toString(Bytes.toBytes("7615"))
+    }else{
+      "-1"
+    }
+    Row(VID,str7615)
+
+  }
+
   /*
     startTimeStamp:当天最小时间戳
     endTimeStamp : 当天下一天的最小时间戳
     即前闭后开
    */
-  def getSpecialDayData(rowkey:String,startTimeStamp:java.lang.Long,endTimeStamp:java.lang.Long):Boolean = {
+  def getSpecialDayData(rowkey:String,dayInitTimestamp:java.lang.Long, hour:Int):Boolean = {
     var timeStamp = 0L
     try{
       timeStamp = rowkey.split("_")(1).toLong
@@ -85,21 +101,14 @@ class SparkLoadHbaseDataDemo1(@transient private var sc:SparkContext,executeDate
       }
 
     }
-    if(timeStamp >= startTimeStamp && timeStamp < endTimeStamp){
+    val startTimestamp  = dayInitTimestamp + hour* 3600000
+    val endTimestamp = startTimestamp + 3600000
+    if(timeStamp >= startTimestamp && timeStamp < endTimestamp){
       true
     }else{
       false
     }
   }
-
-  def getRegexRowkeyFilter() : Filter = {
-     val timestampPrefix = DateUtil.getFilterDatePrefix(executeDate)
-     val regexKey = s"[\\dA-Fa-f]{32}_((${timestampPrefix._1}|${timestampPrefix._2})[\\d]{8}){1}"
-     val regexStringComparator = new RegexStringComparator(regexKey)
-     val rowFilter = new RowFilter(CompareFilter.CompareOp.EQUAL, regexStringComparator)
-     rowFilter
-  }
-
 
 }
 
@@ -115,7 +124,7 @@ object SparkLoadHbaseDataDemo1 {
        val sc = new SparkContext(sparkConf)
        val executeDate = args(0)
        val obj = new SparkLoadHbaseDataDemo1(sc,executeDate)
-       obj.loadDataFromHbase()
+       obj.loadDataFromHbase1()
      }
 }
 
